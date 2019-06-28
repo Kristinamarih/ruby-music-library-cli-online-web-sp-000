4ca5741e48373a4e3a0d5cdc4257fa8470532ba2
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-28 13:32:08 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -106,10 +106,10 @@ end
 
 def play_song 
   puts "Which song number would you like to play?"
-  input = gets.chomp.to_i
   list_songs = Song.all.sort{|a, b| a.name <=> b.name}
-  if (input > 0) && (input <= list_songs.size)
-    
+  input = gets.chomp.to_i
+    if (input > 0) && (input <= list_songs.size)
+    if (input )
       puts "Playing #{song.name} by #{song.artist.name}."
     end
   end
