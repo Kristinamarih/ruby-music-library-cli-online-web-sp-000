5aaac9e4a1d4583278061d46c9da19aa957dc63f
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-28 13:15:34 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -108,8 +108,7 @@ def play_song
   puts "Which song number would you like to play?"
   input = gets.chomp
   list_of_songs = Song.all.sort{|a, b| a.name.upcase <=> b.name.upcase}.each do
-    |song|
-    if song == input 
+    |song| if song == input 
       puts "Playing #{song.name} by #{song.artist.name}."
     end
   end
